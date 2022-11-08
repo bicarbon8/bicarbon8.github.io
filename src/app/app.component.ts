840f763611b1c3af70e20c18d63517579bbebd8e
Jason Holt Smith <jason.smith@verizonconnect.com>
2022-11-08 14:37:45 +0000

    support theme selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+import { Theme } from './theme';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bicarbon8-github-io';
+export class AppComponent implements AfterContentInit {
+  public theme: Theme;
+
+  ngAfterContentInit() {
+    this.theme ??= 'default';
+    this.setTheme();
+  }
+
+  onThemeUpdated(event) {
+    this.theme = event.theme;
+    this.setTheme();
+  }
+
+  setTheme() {
+    const el = document.getElementById('app-container');
+    el.classList.remove('theme-default', 'theme-forest', 'theme-glacier', 'theme-watermelon');
+    el.classList.add(`theme-${this.theme}`);
+  }
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,8 +1,44 @@
+import { Theme } from "./theme";
+
+const pkgname = require("../../package.json").name;
+
+type AppData = {
+    theme: Theme;
+};
+
 export module utils {
     export var shuffle = (array: Array<any>) => {
         return array
             .map(value => ({ value, sort: Math.random() }))
             .sort((a, b) => a.sort - b.sort)
             .map(({ value }) => value);
+    }
+
+    export var getData = <K extends keyof AppData, V extends AppData[K]>(key: K, defaultVal?: V): V => {
+        try {
+            const strData = localStorage.getItem(pkgname);
+            if (strData) {
+                const data: AppData = JSON.parse(strData);
+                return data[key] as V ?? defaultVal;
+            }
+        } catch(e) {
+            return defaultVal;
+        }
+    }
+
+    export var setData = <K extends keyof AppData, V extends AppData[K]>(key: K, val: V): void => {
+        let data: AppData;
+        try {
+            const strData = localStorage.getItem(pkgname);
+            if (strData) {
+                data = JSON.parse(strData);
+            } else {
+                data = {theme: null};
+            }
+        } catch(e) {
+            data = {theme: null};
+        }
+        data[key] = val;
+        localStorage.setItem(pkgname, JSON.stringify(data));
     }
 }
