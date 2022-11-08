1cef6cd2dfd51f1fe11a2732c59785de342a935d
Jason Holt Smith <bicarbon8@gmail.com>
2022-11-08 14:48:43 +0000

    Add themes (#15)
    * creating theme variables and mapping these to site
    * cleanup of unused stylings
    * support theme selection
    * better aligning the theme selector in nav
    
    Co-authored-by: Jason Holt Smith <jason.smith@verizonconnect.com>

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { SiteMap } from './site-map';
 import { NavigationService } from './navigation.service';
+import { Theme } from '../theme';
+import { utils } from '../utils';
 
 @Component({
   selector: 'app-navigation',
@@ -10,10 +12,14 @@ import { NavigationService } from './navigation.service';
 export class NavigationComponent implements OnInit {
   public siteMap: SiteMap;
   public dataLoaded: boolean = false;
+  public selectedTheme: Theme;
+  @Output()
+  themeUpdated = new EventEmitter<{theme: Theme}>();
   
   constructor(private _navService: NavigationService) { }
 
   ngOnInit(): void {
+    this.setSavedThemeAsSelected();
     this._navService.getSiteMap()
       .subscribe((smap: SiteMap) => {
         this.siteMap = smap;
@@ -21,7 +27,32 @@ export class NavigationComponent implements OnInit {
       });
   }
 
+  setSavedThemeAsSelected() {
+    const theme = this.getSavedTheme();
+    if (theme) {
+      const select = document.getElementById('theme-select') as HTMLSelectElement;
+      const option = select.querySelector(`[value='${theme}']`) as HTMLOptionElement;
+      option.selected = true;
+      this.themeUpdated.emit({theme: theme});
+    }
+  }
+
+  handleThemeChange(event) {
+    const select = event.target as HTMLSelectElement;
+    const theme: Theme = select.options[select.selectedIndex].value as Theme;
+    this.saveTheme(theme);
+    this.themeUpdated.emit({theme: theme});
+  }
+
   goTo(url: string, target: string = '_self'): void {
     window.open(url, target);
+  }
+
+  getSavedTheme(): Theme {
+    return utils.getData('theme');
+  }
+
+  saveTheme(theme: Theme): void {
+    utils.setData('theme', theme);
   }
 }
