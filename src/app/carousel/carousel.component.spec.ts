b9a5931d1d7560771320f4a882949a63e569ea7a
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-07-06 17:04:25 +0100

    fixes carousel component tests

--- src/app/carousel/carousel.component.spec.ts
+++ src/app/carousel/carousel.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CarouselComponent } from './carousel.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('CarouselComponent', () => {
   let component: CarouselComponent;
   let fixture: ComponentFixture<CarouselComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
+      declarations: [ CarouselComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   });
@@ -16,6 +19,7 @@ describe('CarouselComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
+    httpClient = TestBed.inject(HttpClient); // Injecting the HttpClient
     fixture.detectChanges();
   });
 
