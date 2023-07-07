943d60e61af52813b5a980875104cc66e2b56f71
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-07-07 10:22:01 +0100

    fixes navigation component and service unit tests

--- src/app/navigation/navigation.component.spec.ts
+++ src/app/navigation/navigation.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      declarations: [ NavigationComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationComponent);
+    httpClient = TestBed.inject(HttpClient);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
