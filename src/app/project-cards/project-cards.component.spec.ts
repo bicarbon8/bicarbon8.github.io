ff7fac27adbb29715cefc2ecee5b46c896cb4cf7
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-07-07 10:25:36 +0100

    fixes project cards component and service unit tests

--- src/app/project-cards/project-cards.component.spec.ts
+++ src/app/project-cards/project-cards.component.spec.ts
@@ -1,20 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectCardsComponent } from './project-cards.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProjectCardsComponent', () => {
   let component: ProjectCardsComponent;
   let fixture: ComponentFixture<ProjectCardsComponent>;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProjectCardsComponent ]
+      declarations: [ ProjectCardsComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectCardsComponent);
+    httpClient = TestBed.inject(HttpClient);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/project-cards/project-cards.service.spec.ts
+++ src/app/project-cards/project-cards.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectCardsService } from './project-cards.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProjectCardsService', () => {
   let service: ProjectCardsService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(ProjectCardsService);
   });
 
